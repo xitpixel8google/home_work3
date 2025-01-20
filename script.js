6f47a4d942970ca0465a736855d021de359acdfe
xitpixel8google <xitpixel8@gmail.com>
2025-01-20 22:43:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict"
+
+
+// 1
+
+// while (true) {
+//     let num = +prompt()
+    
+//     if (num <= 1) {
+//         alert(`${num} banana`)
+//     } else if (num > 1) {
+//         alert(`${num} bananas`)
+//     } else {
+//         alert('Error')
+//     }
+// }
+
+
+// 2
+
+// let n = +prompt()
+// let result = 0
+
+// for (let i = 0; i <= n; i++) {
+//     result += i
+// }
+
+// alert(result)
+
+
+// 3
+
+// let x = +prompt()
+// let n = +prompt()
+// let result = 1
+
+// for (let i = 0; i < n; i++) {
+//     result *= x
+// }
+
+// alert(result)
+
